99cc46e8ce7f6d827974d68f0512e3f9e53870a6
Bill Orcutt <orcutt@gmail.com>
2015-09-11 15:05:15 -0700

    Create a stub midi file

--- src/midi.js
+++ src/midi.js
@@ -1,5 +1,7 @@
 //midi access
-cracked.midi_access = null;
+cracked._midi_access = null;
+cracked._midi_inputs = null;
+cracked._midi_outputs = null;
 
 //is midi supported?
 cracked.midi_supported = function(){
@@ -9,40 +11,52 @@ cracked.midi_supported = function(){
 //receive midi. callback is called on midi received event.
 // if id is omitted then listens on all inputs
 cracked.midi_receive = function(callback, id){
-    if(cracked.midi_access) {
-
+    if((__._midi_access || __.midi_init()) && __.isFun(callback)) {
+        var inputs = __._midi_inputs.values();
+        // loop over all available inputs and listen for any MIDI input
+        for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
+            // each time there is a midi message call the onMIDIMessage function
+            input.value.onmidimessage = callback;
+        }
+    } else {
+        console.error("midi_receive: midi not available");
     }
 };
 
 //send midi. takes a data object.
 // if id is omitted then sends to all outputs
 cracked.midi_send = function(data, id){
-    if(cracked.midi_access) {
+    if(__._midi_access || __.midi_init()) {
 
+    } else {
+        console.error("midi_send: midi not available");
     }
 };
 
 //returns an object w/ current midi status
 cracked.midi_status = function() {
-    if(cracked.midi_access) {
+    if(__._midi_access || __.midi_init()) {
         return {};
     } else {
+        console.error("midi_status: midi not available");
         return null;
     }
 };
 
 //initialize midi
 cracked.midi_init = function() {
-    if(__.midi_supported) {
+    if(__.midi_supported && !__._midi_access) {
         navigator.requestMIDIAccess().then( function() {
-           __.midi_access = access;
+            __._midi_access = access;
+            __._midi_inputs = access.inputs; // inputs = MIDIInputMaps, you can retrieve the inputs with iterators
+            __._midi_outputs = access.outputs; // outputs = MIDIOutputMaps, you can retrieve the outputs with iterators
             return true;
         }, function( err ) {
             console.error( "midi_init: Initialization failed. Error code: " + err.code );
             return false;
         } );
     } else {
-        console.error("Midi not supported by your browser");
+        console.error("midi_init: Failed. Midi not supported by your browser");
         return false;
     }
 };
